816a817bc82fc2fa283f6b7958ce704188555213
HanSeongJun <gkstjdwns2@naver.com>
2021-03-25 14:34:26 +0900

    로그인, 로그아웃 기능

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require('express')
 const app = express()
 const port = 5000 
 const bodyParser = require('body-parser');
-const { User } = require("./models/User");
+const cookieParser = require('cookie-parser');
 const config = require('./config/key')
-// application/x-www-form-urlencoded 의 형식의 데이터를 가져와서 분석해준다.
-app.use(bodyParser.urlencoded({extended: true}));
+const { auth } = require('./middleware/auth')
+const { User } = require("./models/User");
 
-// application/json 의 형식의 데이터를 가져와서 분석해준다.
-app.use(bodyParser.json());
-
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
+
 mongoose.connect(config.mongoURI , {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 })
@@ -20,20 +21,86 @@ mongoose.connect(config.mongoURI , {
 
 app.get('/' , (req, res) => res.send('Hello World. hi'))
 
-
-
-app.post('/register', (req, res) =>{
-    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+app.post('/api/users/register', (req, res) => {
+    // 회원가입을 할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 DB에 넣어준다.
     const user = new User(req.body)
-    user.save((err, userInfo) => {
-        if(err) return res.json({success: false, err})
-        return res.status(200).json({
+
+    user.save( (err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json( {
             success: true
+        })
+    })
+
+})
+
+app.post('/api/users/login', (req, res) => {
+    // 요청된 이메일이 DB에 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if( !user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        // 요청된 이메일이 DB에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+        user.comparePassword(req.body.password, (err, isMatch ) => {
+            if(!isMatch)
+            return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+            user.generateToken( (err, user) => {
+                if(err) return res.status(400).send(err);
+
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id })
+            })
         })
     })
 })
 
+app.post('/api/users/auth', auth, (req, res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 이야기는 Authentication이 Ture라는 말.
+    res.status(200).json({
+        // user 정보를 제공
+        _id: req.user._id,
+        // role=0 -> 일반 유저, role이 0이 아닌 경우에는 관리자 
+        isAdmin: req.user.role == 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+}) 
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+    // user 찾기
+    User.findOneAndUpdate({ _id: req.user._id},
+        { token: ""} // 로그아웃을 했으니 토큰을 제거 
+        , (err, user) => {
+            if(err) return res.json({success: false, err});
+            return res.status(200).send( {
+                success: true
+            })
+        })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
-
